e12423cedfef9c41224832e2aae36372f6a48799
skmeetaali <mitalikamble5@gmail.com>
2026-05-29 11:59:55 +0530

    a separate class for normal user - sets the role as USER & a special normalusermanager class for filtering role based users - class derieved from BaseUSermanager

--- she_can/authentication/models.py
+++ she_can/authentication/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 class people(AbstractUser):
@@ -12,7 +12,27 @@ class people(AbstractUser):
     
     def save(self, *args, **kwargs):
         if not self.pk :
-            self,role = self.base_role
+            self.role = self.base_role
             return super().save(*args, **kwargs)
         
+class NormalUser(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        all_user =  super().get_queryset(*args, **kwargs)
+        return all_user.filter(role=people.Role.USER)
+
+class user(people):
+    base_role = people.Role.USER
+    user = NormalUser()
+    class Meta:
+        proxy = True
         
+    def welcome(self):
+        return "Welcome to SHeCan Foundation"
+    
+class NormalUserProfile(models.Model):
+    user = models.OneToOneField(user, on_delete=models.CASCADE)
+    name = models.CharField(True)
+    age = models.IntegerField(null = True, blank=True)
+
+
+    
