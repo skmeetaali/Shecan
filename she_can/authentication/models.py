b52a40b7ae68e2510bb4deba09e0f2ad00011ff2
skmeetaali <mitalikamble5@gmail.com>
2026-05-29 10:22:22 +0530

    ADMIN_USER_MODEL changed to 'auth.../people'

--- she_can/authentication/models.py
+++ she_can/authentication/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class people(AbstractUser):
+    class Role(models.TextChoices):
+       ADMIN = "ADMIN" , 'admin'
+       USER = "USER", 'user'
+       
+    base_role = Role.ADMIN
+    role = models.CharField(max_length=50, choices = Role.choices)
+    
+    def save(self, *args, **kwargs):
+        if not self.pk :
+            self,role = self.base_role
+            return super().save(*args, **kwargs)
+        
+        
